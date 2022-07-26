5429475b61fd96a2a94a9db331b586afbd28760b
Francis01 <kouakoufrancis809@gmail.com>
2022-07-26 17:27:26 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import routes from './src/routes/crmRoutes.js';
+
+const app = express();
+const PORT= process.env.PORT || 3500;
+
+
+//mongo connexion
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+    useNewUrlParser: true
+});
+
+//body parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//routes 
+routes(app);
+
+//public middleware
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.send("Hello word");
+});
+
+
+
+app.listen(PORT, () => console.log(`Server start with port ${PORT}`));
